0f96f46548612d66f26c044d75b0b9c154160b28
Angel Velasquez <pcisavel@upc.edu.pe>
2023-04-17 20:50:56 -0500

    chore: added base configuration for ui components.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,51 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from "primevue/config";
+
+// PrimeVue Material Design Theme
+import 'primevue/resources/themes/md-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+
+// Toast Service
+import ToastService from 'primevue/toastservice';
+
+// PrimeVue Components
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import Toolbar from "primevue/toolbar";
+import InputText from "primevue/inputtext";
+import Textarea from "primevue/textarea";
+import Button from "primevue/button";
+import Row from "primevue/row";
+import Sidebar from "primevue/sidebar";
+import Menu from "primevue/menu";
+import Dialog from "primevue/dialog";
+import Toast from "primevue/toast";
+import Dropdown from "primevue/dropdown";
+import Tag from "primevue/tag";
+import Card from "primevue/card";
+
+createApp(App)
+    .use(PrimeVue, { ripple: true })
+    .use(ToastService)
+    .component('pv-data-table', DataTable)
+    .component("pv-column", Column)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-input-text', InputText)
+    .component('pv-textarea', Textarea)
+    .component('pv-button', Button)
+    .component('pv-row', Row)
+    .component('pv-sidebar', Sidebar)
+    .component('pv-menu', Menu)
+    .component('pv-dialog', Dialog)
+    .component('pv-toast', Toast)
+    .component('pv-dropdown', Dropdown)
+    .component('pv-tag', Tag)
+    .component('pv-card', Card)
+    .mount('#app')
+
+
